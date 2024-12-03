cce9ae268be1a6737981d8b06942d3202101b143
TenE18 <tchinnu5132@gmail.com>
2024-12-03 09:41:34 +0530

    add storybook and tailwind css support

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,7 +1,9 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
-export const Button =({children}:{children:ReactNode})=>{
-return(
-    <button>{children}</button>
-)
-}
+export const Button = ({ children }: { children: ReactNode }) => {
+  return (
+    <button className="p-4 bg-gray-500 text-white font-bold tracking-wide rounded-md shadow ">
+      {children}
+    </button>
+  );
+};
